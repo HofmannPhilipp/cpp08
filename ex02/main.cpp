5de15cef3fe4ab0006dd655351a5c98ad951864d
Philipp Hofmann <phhofman@3-F-13.42heilbronn.de>
2025-09-17 15:49:55 +0200

    added same tests but with list container

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,21 +6,57 @@
 /*   By: phhofman <phhofman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/17 15:22:17 by phhofman          #+#    #+#             */
-/*   Updated: 2025/09/17 15:23:04 by phhofman         ###   ########.fr       */
+/*   Updated: 2025/09/17 15:48:18 by phhofman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
 #include <iostream>
+#include <list>
 
-int main(void)
+void listTest()
 {
+    std::cout << "===== Same Tests with List Container =====" << std::endl;
+
+    std::list<int> lst;
+    lst.push_back(5);
+    lst.push_back(17);
+    std::cout << "[List] Last element: " << lst.back() << std::endl;
+
+    lst.pop_back();
+    std::cout << "[List] Size after pop: " << lst.size() << std::endl;
+
+    lst.push_back(3);
+    lst.push_back(5);
+    lst.push_back(737);
+    //[...]
+    lst.push_back(0);
+    std::list<int>::iterator it = lst.begin();
+    std::list<int>::iterator ite = lst.end();
+    ++it;
+    --it;
+
+    std::cout << "[List] Elements:" << std::endl;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+}
+
+void mutantTest()
+{
+    std::cout << "===== MutantStack Tests =====" << std::endl;
+
     MutantStack<int> mstack;
     mstack.push(5);
     mstack.push(17);
-    std::cout << mstack.top() << std::endl;
+
+    std::cout << "[MutantStack] Top element: " << mstack.top() << std::endl;
+
     mstack.pop();
-    std::cout << mstack.size() << std::endl;
+    std::cout << "[MutantStack] Size after pop: " << mstack.size() << std::endl;
+
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
@@ -30,11 +66,19 @@ int main(void)
     MutantStack<int>::iterator ite = mstack.end();
     ++it;
     --it;
+
+    std::cout << "[MutantStack] Elements:" << std::endl;
     while (it != ite)
     {
         std::cout << *it << std::endl;
         ++it;
     }
     std::stack<int> s(mstack);
+}
+
+int main(void)
+{
+    mutantTest();
+    listTest();
     return 0;
 }
